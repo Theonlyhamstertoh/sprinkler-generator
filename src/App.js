6f8d8f3e9d52703029d77f913b3f027f5e69b3bd
Weibo Zhang <75579372+Theonlyhamstertoh@users.noreply.github.com>
2021-06-27 19:12:22 -0700

    made a cool sin cos graph

--- src/App.js
+++ src/App.js
@@ -1,81 +1,98 @@
 import "./styles.css";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, Html } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Suspense, useCallback, useMemo, useState } from "react";
+
+import { useMemo } from "react";
 import * as THREE from "three";
 
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 4, 4] }}>
       <color args={["#222"]} attach="background" />
-      <gridHelper args={20} />
+
       <OrbitControls />
       <Particles />
-      <axesHelper args={10} position={[0, 0.001, 0]} />
     </Canvas>
   );
 }
 
+const MAX_PARTICLES = 100000;
 function Particles() {
-  const { particles, radius } = useControls({
+  const {
+    particles,
+    radius,
+    branch,
+    randomPower,
+    innerColor,
+    outsideColor,
+    spin
+  } = useControls({
     particles: {
-      max: 1000,
-      min: 10,
-      step: 10,
-      value: 100
+      value: 5000,
+      min: 100,
+      max: MAX_PARTICLES,
+      step: 100
     },
-    radius: {
-      min: 1,
-      max: 10,
-      step: 1,
-      value: 2
-    },
-    angle: {}
+    radius: { value: 10, max: 100, min: 1, step: 1 },
+    spin: { value: 0.5, max: 1, min: 0, step: 0.01 },
+    branch: { value: 4, max: 50, min: 1, step: 1 },
+    randomPower: { value: 16, max: 64, min: 1, step: 1 },
+    innerColor: "#ff0000",
+    outsideColor: "#0000ff"
   });
-
-  const [positions, colors] = useMemo(() => {
-    const positions = new Float32Array(particles * 3);
-    const colors = new Float32Array(particles * 3);
+  const [coords, colors] = useMemo(() => {
+    const coords = new Float32Array(MAX_PARTICLES * 3);
+    const colors = new Float32Array(MAX_PARTICLES * 3);
+    const colorInside = new THREE.Color(innerColor);
+    const colorOutside = new THREE.Color(outsideColor);
 
     for (let i = 0; i < particles; i++) {
       const i3 = i * 3;
-      const radiusValue = Math.random() * radius;
-      const angleValue = Math.random();
-      positions[i3] = Math.cos(Math.PI * 2) * radiusValue;
-      positions[i3 + 1] = 0;
-      positions[i3 + 2] = 0;
+      const radiusValue = (Math.random() - 0.5) * radius;
+      const branchValue = ((i % branch) / branch) * Math.PI * 2;
+      const spinAngle = spin * radiusValue;
 
-      colors[i3] = Math.random();
-      colors[i3 + 1] = Math.random();
-      colors[i3 + 2] = Math.random();
+      const randomY =
+        Math.pow(Math.random(), randomPower) * (Math.random() < 0.5 ? 1 : -1);
+
+      coords[i3] = Math.cos(branchValue + spinAngle) * radiusValue;
+      coords[i3 + 1] = randomY;
+      coords[i3 + 2] = Math.sin(branchValue + spinAngle) * radiusValue;
+
+      const mixedColor = colorInside.clone();
+      mixedColor.lerp(colorOutside, radiusValue / radius);
+      colors[i3 + 0] = mixedColor.r;
+      colors[i3 + 1] = mixedColor.g;
+      colors[i3 + 2] = mixedColor.b;
     }
 
-    return [positions, colors];
-  }, [particles]);
+    return [coords, colors];
+  }, [particles, radius, branch, randomPower, innerColor, outsideColor, spin]);
 
   return (
-    <points key={particles}>
+    <points>
       <bufferGeometry>
         <bufferAttribute
           attachObject={["attributes", "position"]}
-          count={particles / 3}
+          count={coords.length / 3}
+          array={coords}
           itemSize={3}
-          array={positions}
+          onUpdate={(self) => (self.needsUpdate = true)}
         />
         <bufferAttribute
           attachObject={["attributes", "color"]}
-          count={particles / 3}
-          itemSize={3}
+          count={colors.length / 3}
           array={colors}
+          itemSize={3}
+          onUpdate={(self) => (self.needsUpdate = true)}
         />
       </bufferGeometry>
       <pointsMaterial
-        size="0.2"
         blending={THREE.AdditiveBlending}
-        depthWrite={false}
+        size="0.2"
         vertexColors={true}
+        sizeAttenuation={true}
       />
     </points>
   );
